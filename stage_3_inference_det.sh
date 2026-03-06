6ce2521c39923c376af265f3d8ecc31fdfb08830
penonbabis798-beep <PenonBabis798@gmail.com>
2026-03-06 08:05:24 +0000

    Final upload of M2F2 Stage 3 and project structure

--- stage_3_inference_det.sh
+++ stage_3_inference_det.sh
@@ -4,6 +4,11 @@ source ~/.bashrc
 current_path=$(pwd)
 export PYTHONPATH="$current_path:$PYTHONPATH"
 
+# --- 关键修改：强制离线模式，直接使用本地缓存的 CLIP 模型 ---
+export TRANSFORMERS_OFFLINE=1
+export HF_DATASETS_OFFLINE=1
+
+# 指定运行的显卡
 CUDA_NUM=0
 CUDA_VISIBLE_DEVICES=$CUDA_NUM python -m llava.serve.cli_DDVQA_det \
-    --model-path /data/tangchengwen/Deepfake视频检测/M2F2_Det/checkpoints/deepfake-M2F2-Det-Final
+    --model-path ./checkpoints/llava-M2F2-Det-Final
--- stage_3_inference_exp.sh
+++ stage_3_inference_exp.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 source ~/.bashrc
-conda activate M2F2_det
+
 current_path=$(pwd)
 export PYTHONPATH="$current_path:$PYTHONPATH"
 
-CUDA_NUM=1
+# --- 关键修改：强制离线模式 ---
+export TRANSFORMERS_OFFLINE=1
+export HF_DATASETS_OFFLINE=1
+
+# 指定运行的显卡
+CUDA_NUM=0
 CUDA_VISIBLE_DEVICES=$CUDA_NUM python -m llava.serve.cli_DDVQA_exp \
-    --model-path ./checkpoints/llava-v1.5-7b-M2F2-Det
+    --model-path ./checkpoints/llava-M2F2-Det-Final
